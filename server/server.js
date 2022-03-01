3d55d50eefecfe5c966fcd2ca1f2768b437bb55b
farhanmian <farhanmian099@gmail.com>
2022-03-01 16:30:10 +0530

    fix(fixed a bug): fixed a bug
    fixed a bug

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const redirectUriPath = "http://localhost:3000";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
